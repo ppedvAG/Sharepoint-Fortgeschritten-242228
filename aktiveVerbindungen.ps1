62339bfe1535fd1d8f3ecc4d0638eea9f49e0b18
Alexander Schwarz <alexanders@ppedv.de>
2025-01-06 10:54:26 +0100

    s1

---
+++ aktiveVerbindungen.ps1
@@ -0,0 +1,16 @@
+﻿Add-PSSnapin *share*
+$servername = "sp1" #FrontendServer
+$WebSiteName="sharepoint - 80" #Web site name in IIS
+Get-Counter -Counter "Webdienst($WebSiteName)\aktuelle verbindungen" -ComputerName $ServerName
+
+
+$servername = "shp19-1"
+Get-Counter -Counter 'web service(_total)\current connections' -ComputerName $servername
+Get-Counter -Counter 'webdienst(_total)\Aktuelle Verbindungen' -ComputerName $servername
+
+
+Add-PSSnapin *share*
+$servername = "shp19-1,shp19-2" #FrontendServer
+$WebSiteName="sharepoint - 80" #Web site name in IIS
+Get-Counter -Counter "web service($WebSiteName)\current connections" -ComputerName $ServerName
+
---
+++ kerberos.ps1
@@ -0,0 +1,22 @@
+﻿#für SQL-Server
+setspn -s MSSQLSvc/sp1:1433 sample\sqlservice				
+setspn -s MSSQLSvc/sp1.sample.local:1433 sample\sqlservice
+
+#für Webanwendung
+setspn -s HTTP/sp1 sample\sp-service 			
+setspn -s HTTP/sp1.sample.local sample\sp-service
+
+#für ZentralAdministration
+setspn -s HTTP/s1:11111 sample\sp-farm 				
+setspn -s HTTP/sp1.schulung.local:5331 sample\sp-farm
+
+#für MysiteHost
+#setspn -s HTTP/mysite svc-spmyapppool
+#setspn -s HTTP/mysite.sample.local svc-spmyapppool
+
+#In der ZA bei den Webanwendungen die Auth. auf "Kerberos" umstellen
+#Danach prüfen, ob im IIS die Auth bei Windowsauth.-> Anbieter auf "Negotiate" steht!
+#Mit klist prüfen ob Ticket ausgestellt wurde
+#Mit klist purge Tickets löschen
+
+#Eventlog IDs mit 4624 prüfen!
